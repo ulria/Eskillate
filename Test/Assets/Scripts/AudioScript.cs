f36b79f548dc95eaf983e216a3c5e29856704222
Ulric Villeneuve <ulric.villeneuve@polymtl.ca>
2019-02-21 18:13:05 -0500

    We now play sounds when successfully placing an object and when incorrectly placing an object! There are insanely loud, we need to decrease the volume.

---
+++ Test/Assets/Scripts/AudioScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    public AudioClip SuccessfulClip;
+    public AudioClip FailClip;
+    public AudioSource Source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlaySuccessful()
+    {
+        if(Source.clip != SuccessfulClip)
+        {
+            Source.clip = SuccessfulClip;
+        }
+        Source.Play();
+    }
+
+    public void PlayFail()
+    {
+        if (Source.clip != FailClip)
+        {
+            Source.clip = FailClip;
+        }
+        Source.Play();
+    }
+}
