c7fddfce4f46327fe5c9b11defecd257657b3565
Ulric Villeneuve <ulric.villeneuve@hotmail.com>
2019-04-18 21:22:15 -0400

    Fixed UI bugs in MiniGameSelection scene. Finished this scene. Missing a video for every miniGame. Added protection if VideoClip not set. Now get most of the GameObjects in the start method instead of by UI for better protection.

--- Test/Assets/Scripts/MiniGame.cs
+++ Test/Assets/Scripts/MiniGame.cs
@@ -9,10 +9,26 @@ public class MiniGame : MonoBehaviour
     public string Description;
     public string LevelSelectionSceneName;
 
-    public GameObject MiniGameDescriptionObject;
-    public GameObject MiniGameNameObject;
+    private GameObject MiniGameDescriptionObject;
+    private GameObject MiniGameNameObject;
     public VideoClip MiniGamePreviewClip;
-    
+    private GameObject MiniGameVideoPreviewPlayer;
+    private GameObject MiniGamePreviewScreen;
+    private RawImage MiniGamePreviewScreenImage;
+
+    private RenderTexture _text;
+    private VideoPlayer _player;
+
+    void Start()
+    {
+        MiniGameDescriptionObject = GameObject.Find("MiniGameDescription");
+        MiniGameNameObject = GameObject.Find("MiniGameName");
+        MiniGamePreviewScreen = GameObject.Find("MiniGamePreviewScreen");
+        MiniGamePreviewScreenImage = MiniGamePreviewScreen.GetComponent<RawImage>();
+        MiniGameVideoPreviewPlayer = GameObject.Find("MiniGameVideoPreviewPlayer");
+        _player = MiniGameVideoPreviewPlayer.GetComponent<VideoPlayer>();
+    }
+
     public void OnSelected()
     {
         DisplayVideoPreview();
@@ -21,7 +37,14 @@ public class MiniGame : MonoBehaviour
 
     private void DisplayVideoPreview()
     {
+        if (MiniGamePreviewClip != null)
+        {
+            _player.clip = MiniGamePreviewClip;
+            _text = new RenderTexture((int)_player.clip.width, (int)_player.clip.height, 0);
+        }
 
+        _player.targetTexture = _text;
+        MiniGamePreviewScreenImage.texture = _text;
     }
 
     private void DisplayDescription()
--- Test/Assets/Scripts/MiniGameSelection.cs
+++ Test/Assets/Scripts/MiniGameSelection.cs
@@ -1,34 +1,39 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MiniGameSelection : MonoBehaviour
 {
     private bool isDragging = false;
-    public float SelectedElementX = 0.0f;
-    public float SelectedElementY = 0.0f;
     private float _elementWidthPlusSpacing = 0.0f;
     private float _scrollRange = 0.0f;
     private float _scrollMin = 0.0f;
-    private int _previouslySelectedMiniGameIndex = 0;
+    private int _previouslySelectedMiniGameIndex = -1;
 
-    public GameObject ScrollList;
-    public GameObject ScrollListContent;
-    public GameObject ScrollListViewport;
+    private GameObject ScrollListContent;
+    private GameObject ScrollListViewport;
     public GameObject[] Elements;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Initializin game objects
+        ScrollListContent = GameObject.Find("ScrollListContent");
+        ScrollListViewport = GameObject.Find("ScrollListViewport");
+
+        // Initializing UI components
         var elementWidth = ScrollListContent.transform.GetChild(0).GetComponent<RectTransform>().rect.width;
         var spacing = ScrollListContent.GetComponent<HorizontalLayoutGroup>().spacing;
         _elementWidthPlusSpacing = elementWidth + spacing;
 
         _scrollRange = (Elements.Length-1) * _elementWidthPlusSpacing;
         _scrollMin = ScrollListContent.GetComponent<RectTransform>().anchoredPosition.x - _scrollRange;
+        _scrollMin /= 2;
 
         ScrollListContent.GetComponent<HorizontalLayoutGroup>().padding.right = (int) (ScrollListViewport.GetComponent<RectTransform>().rect.width - (elementWidth + spacing));
 
-        Elements[0].GetComponent<MiniGame>().OnSelected();
+        var newContentPos = new Vector2(_scrollMin + _scrollRange, 0);
+        ScrollListContent.GetComponent<RectTransform>().anchoredPosition = newContentPos;
     }
 
     // Update is called once per frame
@@ -36,19 +41,26 @@ public class MiniGameSelection : MonoBehaviour
     {
         if(!isDragging)
         {
+            // Find out which MiniGame is in the Selection spot
             var currentScrollX = ScrollListContent.GetComponent<RectTransform>().anchoredPosition.x;
             var scrollOffset = currentScrollX - _scrollMin;
             var scrollNbElementOffset = Mathf.RoundToInt(scrollOffset / _elementWidthPlusSpacing);
             var newOffset = scrollNbElementOffset * _elementWidthPlusSpacing;
             var newPosition = _scrollMin + newOffset;
             newPosition = Mathf.Clamp(newPosition, _scrollMin, _scrollMin + _scrollRange);
+            // Snap to this MiniGame
             LerpToElement(newPosition);
 
+            // Invert index, because, for some reason index 0 represents the left most element
             var elementIndexReversed = (Elements.Length - 1) - scrollNbElementOffset;
+            // Call the OnSelected method of the MiniGame in the selection spot
             if (elementIndexReversed != _previouslySelectedMiniGameIndex)
             {
-                Elements[elementIndexReversed].GetComponent<MiniGame>().OnSelected();
-                _previouslySelectedMiniGameIndex = elementIndexReversed;
+                if(elementIndexReversed >= 0 && elementIndexReversed < Elements.Length)
+                {
+                    Elements[elementIndexReversed].GetComponent<MiniGame>().OnSelected();
+                    _previouslySelectedMiniGameIndex = elementIndexReversed;
+                }
             }
         }
     }
@@ -56,7 +68,7 @@ public class MiniGameSelection : MonoBehaviour
     private void LerpToElement(float position)
     {
         var newX = Mathf.Lerp(ScrollListContent.GetComponent<RectTransform>().anchoredPosition.x, position, Time.deltaTime * 10f);
-        Vector2 newPos = new Vector2(newX, SelectedElementY);
+        Vector2 newPos = new Vector2(newX, 0);
         ScrollListContent.GetComponent<RectTransform>().anchoredPosition = newPos;
     }
 
@@ -73,5 +85,10 @@ public class MiniGameSelection : MonoBehaviour
     public void OnLevelSelectionClicked()
     {
         Elements[_previouslySelectedMiniGameIndex].GetComponent<MiniGame>().OnLevelSelectionClicked();
+    }
+
+    public void OnBackToMainMenuClicked()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
